24e909ac018acbe06ab6b6c17a5118efedb50f45
jduffel1929@hotmail.com <jduffel1929@hotmail.com>
2020-06-24 21:50:59 +0000

    Implement paramitized query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,12 +7,15 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const limit = process.argv[3]
+const cohort_id = process.argv[2]
+
 pool.query(`
 SELECT students.id, students.name, cohorts.name AS cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[3]}%'
-LIMIT '${process.argv[2]}';`)
+WHERE cohorts.name LIKE $2
+LIMIT $1;`, [limit, cohort_id + '%'])
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
